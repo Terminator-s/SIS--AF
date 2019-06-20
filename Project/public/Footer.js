469bdf3f8f2c4c3dab02a6c440e3385af3403da4
Pavidha Lojini Rajendran <38418745+Lojini@users.noreply.github.com>
2019-06-21 01:45:12 +0530

    backend for course management

---
+++ Project/public/Footer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div>
+            <div className="footer">
+              <p>
+                  <ul>
+                      <li>Home </li>
+                      <li>Course</li>
+                      <li>Modules </li>
+                      <li>About Us</li>
+                      <li>FAQ</li>
+
+                  </ul></p>
+
+            </div>
+            <p>Copyright 2018 ©ITI. All Rights Reserved</p>
+        </div>
+        )
+    }
+}
+
+export default Footer;
