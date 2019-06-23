a7e8abd7a0e49492acc12e06d6b144d1a39d8d81
rushdanajeeb <rushdanajeeb@gmail.com>
2019-06-23 10:51:22 +0530

    added instructor get post calls

--- afproject/src/index.js
+++ afproject/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Students/Assessments/sViewAssesments';
+import AdminHome from './Admin/adminHome';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<AdminHome />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
